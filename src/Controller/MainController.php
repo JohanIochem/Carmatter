475521b5e4d9e362c631ae777316cbc84fa3e474
Johan Iochem <112541477+JohanIochem@users.noreply.github.com>
2023-07-23 19:52:46 +0200

    Avencement du projet 🚀🔥

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,18 +1,25 @@
 <?php
 
+// src/Controller/MainController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\Exception\TransportExceptionInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+
+        // Passer les données à votre template Twig
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+
         ]);
     }
 }
